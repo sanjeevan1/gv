e17cdc5cd1b3f3532a28942ada1eb9791ab189a6
sanjee <79707868+sanjeevan1@users.noreply.github.com>
2023-09-23 07:16:12 +0530

    Update db.php
    // Create connection
    $conn = new mysqli($servername, $username, $password,$DBname);
    // Check connection
    if ($conn->connect_error) {
      die("Connection failed: " . $conn->connect_error);
    }
    
    // Create database
    $sql = "CREATE DATABASE myDB";
    if ($conn->query($sql) === TRUE) {
      echo "Database created successfully";
    } else {
      echo "Error creating database: " . $conn->error;
    }
    
    $conn->close();
    ?>

--- db.php
+++ db.php
@@ -4,20 +4,20 @@ $username = "username";
 $password = "password";
 $DBname="xiteb"
 
-// Create connection
-$conn = new mysqli($servername, $username, $password,$DBname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+// // Create connection
+// $conn = new mysqli($servername, $username, $password,$DBname);
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
 
-// Create database
-$sql = "CREATE DATABASE myDB";
-if ($conn->query($sql) === TRUE) {
-  echo "Database created successfully";
-} else {
-  echo "Error creating database: " . $conn->error;
-}
+// // Create database
+// $sql = "CREATE DATABASE myDB";
+// if ($conn->query($sql) === TRUE) {
+//   echo "Database created successfully";
+// } else {
+//   echo "Error creating database: " . $conn->error;
+// }
 
-$conn->close();
-?>
+// $conn->close();
+// ?>
